8ce72a0768d142188c3d3036f53c6986229ff073
knoph1 <knopholuoch@yahoo.com>
2025-01-08 00:12:06 +0300

    Empty repo'

--- backend/api/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('register/', views.register_user),
-    path('token/', views.obtain_token),
-    path('products/', views.ProductListView.as_view()),
-    path('vendor/products/', views.VendorProductView.as_view()),
-]
--- backend/static/settings.py
+++
@@ -1,2 +0,0 @@
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
